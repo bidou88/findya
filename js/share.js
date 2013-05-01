1f33fa310c62a1b75dcce6c5965df8ef69e5d227
Julien Biedermann <julien.biedermann@gmail.com>
2013-05-01 10:31:45 +0200

    mercredi matin

--- js/share.js
+++ js/share.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
-    
+	$('#link').click(function() {
+		window.location.href= $('#link').text();
+	});
+
+	$('#share').click(function() {
+		if (facebook === 'connected') {
+			sendRequestViaMultiFriendSelector();
+		} else {
+			login();
+		}
+	});
 
 });
