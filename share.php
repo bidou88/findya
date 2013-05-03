ada5e819f4f57126712f006222b36dc7bb74399a
Julien Biedermann <julien.biedermann@gmail.com>
2013-05-03 11:06:43 +0200

    js

--- share.php
+++ share.php
@@ -27,71 +27,11 @@
 	<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.css" />
 	<script src="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
 
+	<script src="js/share.js"></script>
 </head>
 <body>
 
 	<div id="fb-root"></div>
-
-	<script>
-
-		var facebook;
-
-		function login() {
-			FB.login(function(response) {
-			    if (response.authResponse) {
-			        sendRequestViaMultiFriendSelector();
-			    } else {
-			        // cancelled
-			    }
-		    });
-		}
-
-		function sendRequestViaMultiFriendSelector() {
-	    	FB.ui({method: 'apprequests',
-        		message: 'Join my map',
-        		title: 'Access the map',
-        		data: $('#link').text(),
-        		frictionlessRequests : true
-      		}, requestCallback);
-    	}
-
-    	function requestCallback(response) {
-          // Handle callback here
-        }
-
-	  window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '158880540947821', // App ID
-	      channelUrl : 'channel.html', // Channel File
-	      status     : true, // check login status
-	      cookie     : true, // enable cookies to allow the server to access the session
-	      xfbml      : true  // parse XFBML
-	    });
-
-	    $(document).trigger("facebook:ready");
-
-	    FB.getLoginStatus(function(response) {
-			if (response.status === 'connected') {
-		    	// connected
-		    	facebook = response.status;
-			} else if (response.status === 'not_authorized') {
-		    	// not_authorized
-		  	} else {
-		    	// not_logged_in
-		  	}
-		});
-
-	  };
-
-	  // Load the SDK Asynchronously
-	  (function(d){
-	     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement('script'); js.id = id; js.async = true;
-	     js.src = "//connect.facebook.net/fr_FR/all.js";
-	     ref.parentNode.insertBefore(js, ref);
-	   }(document));
-	</script>
 
 	<div data-role="page">
 	
